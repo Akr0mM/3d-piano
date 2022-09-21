9946ccf6149ad5563c76bf2fb6b46778a7afc643
Maxime Faivre-Maçon <maxime.fm@gmail.com>
2022-09-21 23:17:43 +0200

    color keys

--- js/Piano.js
+++ js/Piano.js
@@ -1,11 +1,36 @@
 import * as THREE from '../node_modules/three/build/three.module.js';
+import { Key } from './Key.js';
 
 export default class Piano {
     constructor() {
         this.pianoGroup = new THREE.Group();
-        this.notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+        this.keys = [
+            ['C4', 0, 'capslock'],
+            ['C4#', 5, 'a'],
+            ['D4', 10, 'q'],
+            ['D4#', 15, 'z'],
+            ['E4', 20, 's'],
+            ['F4', 30, 'd'],
+            ['F4#', 35, 'r'],
+            ['G4', 40, 'f'],
+            ['G4#', 45, 't'],
+            ['A5', 50, 'g'],
+            ['A4#', 55, 'y'],
+            ['B5', 60, 'h'],
+            ['C5', 70, 'j'],
+            ['C5#', 75, 'i'],
+            ['D5', 80, 'k'],
+            ['D5#', 85, 'o'],
+            ['E5', 90, 'l'],
+            ['F5', 100, 'm'],
+            ['F5#', 105, 'dead'],
+            ['G5', 110, 'ù'],
+            ['G5#', 115, '$'],
+            ['A5', 120, '*'],
+            ['A5#', 125, 'enter'],
+            ['B5', 130, 'shift'],
+        ];
         this.createPiano();
-        this.initEvents()
     }
 
     getMesh() {
@@ -13,45 +38,23 @@ export default class Piano {
     }
 
     createPiano() {
-        let white = 0
-        let black = 0
-        for (let i = 0; i < this.notes.length; i++) {
-            let noteMesh;
-            if (this.notes[i][1] === '#') {
-                noteMesh = new THREE.Mesh(
-                    new THREE.BoxBufferGeometry(1, 3.5, 0.5),
-                    new THREE.MeshBasicMaterial({ color: '#121212' })
-                );
-                noteMesh.position.x = blackPosition(black)
-                noteMesh.position.z = 1
-                black++
-                
-            } else {
-                noteMesh = new THREE.Mesh(
-                    new THREE.BoxBufferGeometry(2, 5, 1),
-                    new THREE.MeshBasicMaterial({ color: '#ccc' })
-                );
-                noteMesh.position.x = white * 2.2;
-                white++
+        this.keys.forEach(key => {
+            let keyMesh = new Key(key).getMesh();
+            this.pianoGroup.add(keyMesh);
+        });
+        this.pianoGroup.position.x -= 65;
+    }
+
+    playKey(keyToplay) {
+        let meshToPlay;
+        let songToPlay;
+        this.keys.forEach(key => {
+            let index = this.keys.indexOf(key)
+            if (key[2] === keyToplay) {
+                meshToPlay = this.pianoGroup.children[index]
+                // songToPlay = `../song/${key[0]}.mp3`
             }
-
-            this.pianoGroup.add(noteMesh);
-        }
-        this.pianoGroup.position.x = - ((white - 1) * 2.2) / 2
+        });
+        meshToPlay.material.color.set(0xff0000)
     }
-
-    initEvents() {
-        console.log(this.getMesh().children);
-    }
-}
-
-function blackPosition(n) {
-    let pos
-    if (n <= 1) {
-        pos = n + 2.1
-    } else if (n <= 4) {
-        pos = 6.6 + n + 0.1
-    }
-    
-    return 1.1
 }
